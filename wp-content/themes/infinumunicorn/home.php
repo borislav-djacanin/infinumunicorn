4e4b53deded9303d243a4d69de15bd445aa076f5
borislav-djacanin <borislav.djacanin@gmail.com>
2017-03-15 07:52:08 +0100

    working files

--- wp-content/themes/infinumunicorn/home.php
+++ wp-content/themes/infinumunicorn/home.php
@@ -8,7 +8,7 @@
 <?php if ( have_posts()) : while ( have_posts() ) : the_post(); ?>
 
 
-	<h3><?php the_title(); ?></h3>
+	<h3 class="boki"><?php the_title(); ?></h3>
 	<?php the_content(); ?>
 	
 <?php endwhile; else: ?>
