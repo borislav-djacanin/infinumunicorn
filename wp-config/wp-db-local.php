3e49636885b858f63df2421da87284ad6ed32175
borislav-djacanin <borislav.djacanin@gmail.com>
2017-03-20 12:21:20 +0100

    Update wp-db-local.php

--- wp-config/wp-db-local.php
+++ wp-config/wp-db-local.php
@@ -2,22 +2,21 @@
 // Prevent file from being accessed directly
 if (!defined('ABSPATH')) exit();
 
-define('DB_NAME',     'infinum');
-define('DB_USER',     '');
-define('DB_PASSWORD', '');
+define('DB_NAME',     'database_name_here');
+define('DB_USER',     'username_here');
+define('DB_PASSWORD', 'password_here');
 define('DB_HOST',     'localhost');
 define('DB_CHARSET',  'utf8');
 define('DB_COLLATE',  '');
 
-
-define('AUTH_KEY',         '');
-define('SECURE_AUTH_KEY',  '');
-define('LOGGED_IN_KEY',    '');
-define('NONCE_KEY',        '');
-define('AUTH_SALT',        '');
-define('SECURE_AUTH_SALT', '');
-define('LOGGED_IN_SALT',   '');
-define('NONCE_SALT',       '');
+define('AUTH_KEY',         'put your unique phrase here');
+define('SECURE_AUTH_KEY',  'put your unique phrase here');
+define('LOGGED_IN_KEY',    'put your unique phrase here');
+define('NONCE_KEY',        'put your unique phrase here');
+define('AUTH_SALT',        'put your unique phrase here');
+define('SECURE_AUTH_SALT', 'put your unique phrase here');
+define('LOGGED_IN_SALT',   'put your unique phrase here');
+define('NONCE_SALT',       'put your unique phrase here');
 
 $table_prefix  = 'bd_wp_';
 
